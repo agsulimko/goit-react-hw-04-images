a75740a4469de0d90cf242a3454dd8fe1a07ec4d
agsulimko <125898308+agsulimko@users.noreply.github.com>
2023-09-21 15:43:21 +0200

    s

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,49 +1,49 @@
 
- import React from 'react'
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
+//  import React from 'react'
+// import Box from '@mui/material/Box';
+// import Button from '@mui/material/Button';
 
-import Modal from '@mui/material/Modal';
+// import Modal from '@mui/material/Modal';
 
-const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  border: 'none',
-  boxShadow: 24,
-  p: 4,
-  padding: 0,
+// const style = {
+//   position: 'absolute',
+//   top: '50%',
+//   left: '50%',
+//   transform: 'translate(-50%, -50%)',
+//   width: 400,
+//   bgcolor: 'background.paper',
+//   border: 'none',
+//   boxShadow: 24,
+//   p: 4,
+//   padding: 0,
 
-};
+// };
 
-export default function BasicModal() {
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+// export default function BasicModal() {
+//   const [open, setOpen] = React.useState(false);
+//   const handleOpen = () => setOpen(true);
+//   const handleClose = () => setOpen(false);
 
-  return (
-    <div>
-      <Button onClick={handleOpen}>Open modal</Button>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
+//   return (
+//     <div>
+//       <Button onClick={handleOpen}>Open modal</Button>
+//       <Modal
+//         open={open}
+//         onClose={handleClose}
+//         aria-labelledby="modal-modal-title"
+//         aria-describedby="modal-modal-description"
+//       >
+//         <Box sx={style}>
          
-          <img src= 
-"https://pixabay.com/get/g9fa88d8f527c93fc8a7a32f6b024c94e43ce0ad33aec7b27556725b8cacb75118bbe0e52abd5d11511f607cb820adbab988faa0ca4988dafcd481afa6fc78855_1280.jpg" alt="Image" style={{ width: '100%',   marginBottom: -50, }} />
+//           <img src= 
+// "https://pixabay.com/get/g9fa88d8f527c93fc8a7a32f6b024c94e43ce0ad33aec7b27556725b8cacb75118bbe0e52abd5d11511f607cb820adbab988faa0ca4988dafcd481afa6fc78855_1280.jpg" alt="Image" style={{ width: '100%',   marginBottom: -50, }} />
             
           
-        </Box>
-      </Modal>
-    </div>
-  );
-}
+//         </Box>
+//       </Modal>
+//     </div>
+//   );
+// }
 
 
 
